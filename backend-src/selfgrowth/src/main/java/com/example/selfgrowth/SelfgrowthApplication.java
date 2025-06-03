843f4048840582d413810eadda862d9411faeeb8
Miyawaki <miyawakifamilymyam@gmail.com>
2025-06-04 00:09:30 +0900

    DB接続

--- backend-src/selfgrowth/src/main/java/com/example/selfgrowth/SelfgrowthApplication.java
+++ backend-src/selfgrowth/src/main/java/com/example/selfgrowth/SelfgrowthApplication.java
@@ -1,13 +1,13 @@
 package com.example.selfgrowth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.selfgrowth.mapper") // これが必要！
 public class SelfgrowthApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SelfgrowthApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SelfgrowthApplication.class, args);
+    }
 }
